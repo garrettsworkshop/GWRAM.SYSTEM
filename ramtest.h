8a90077d1e6408d89cc24460e42cfe1913f891c5
Zane Kaminski <zanekaminski@gmail.com>
2020-09-06 00:00:50 -0400

    Added more test stuff

---
+++ ramtest.h
@@ -0,0 +1,6 @@
+#ifndef TEST_H
+#define TEST_H
+
+char test_run();
+
+#endif /* TEST_H */
---
+++ ramtestpat.c
@@ -0,0 +1,4 @@
+#define RAMTESTPAT_SIZE 4097
+char ramtestpat[RAMTESTPAT_SIZE] = {
+	1, 2, 3, 4, 5, 6, 7, 8, 9
+};
